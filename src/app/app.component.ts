e9e4614d51f6309a3f29c85dc3c72d405ad2a6db
Rajesh K <841998@cognizant.com>
2022-06-01 12:27:18 +0530

    First angular Todo project commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Todo';
+  usersInput: string = '';
+  list: any = [];
+
+  addTodo() {
+    this.list.push(this.usersInput);
+    this.usersInput = '';
+  }
+
+  removeItems(i: number) {
+    console.log(i);
+    console.log(this.list);
+    this.list.splice(i, 1);
+    console.log(this.list);
+
+    // this.list = this.list.filter((ele,index) => i !== index);
+  }
+
+  //splice-in particular index ,delete 1 item
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
